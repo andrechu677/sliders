4daadf8309b4a2f79341948c22f1ed936e7f62e5
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-08-19 00:11:04 +0700

    Update royal138.js

--- royal138.js
+++ royal138.js
@@ -1,19 +1,19 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/611cea6ea2538_slider-welcome-bonus.jpg",
+					"url": "image-royal138/royal138_banner_1.jpg",
 					"alt": "Welcome bonus slot"
 		},
 		{
-					"url": "https://files.sitestatic.net/banners/611cead2c36f4_slider-pulsa.jpg",
+					"url": "image-royal138/royal138_banner_2.jpg",
 					"alt": "Deposit via pulsa"
         },
 		{
-					"url": "https://files.sitestatic.net/banners/611ceb1d4c198_slider-bank.jpg",
+					"url": "image-royal138/royal138_banner_3.jpg",
 					"alt": "Sistem auto Deposit"
         },
 		{
-					"url": "https://files.sitestatic.net/banners/611ceb6d90349_slider-komisi.jpg",
+					"url": "image-royal138/royal138_banner_4.jpg",
 					"alt": "Komisi harian"
 		}
 	]
