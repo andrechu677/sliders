9432ee619767243c56717a97424ee3c5d6f590f5
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-08-18 23:21:49 +0700

    Create royal138.js

---
+++ royal138.js
@@ -0,0 +1,20 @@
+{
+	"items": [
+		{
+					"url": "https://files.sitestatic.net/banners/611cea6ea2538_slider-welcome-bonus.jpg",
+					"alt": "Welcome bonus slot"
+		},
+		{
+					"url": "https://files.sitestatic.net/banners/611cead2c36f4_slider-pulsa.jpg",
+					"alt": "Deposit via pulsa"
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/611ceb1d4c198_slider-bank.jpg",
+					"alt": "Sistem auto Deposit"
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/611ceb6d90349_slider-komisi.jpg",
+					"alt": "Komisi harian"
+		}
+	]
+}
