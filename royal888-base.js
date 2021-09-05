aec08bb2016f617369e1ca26c34e483652ae3855
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-05 23:34:13 +0700

    Create royal888-base.js

---
+++ royal888-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/NAl9FaMIsw4lOr0Ss9GZHNSG0vXgYIXj4CMvavjO.jpeg",
+      "alt": "Promo PSBB"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/SH1ZeFQ2G0nlnaq7IFhnXdbDpbzgcxMqQ25NhDYo.jpeg",
+      "alt": "Deposit Via Pulsa"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/1611748760_sliding-3.jpg",
+      "alt": "Sistem Deposit Tercepat"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/1611918491_sliding-4.jpg",
+      "alt": "Selamat Datang di Royal888"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/7Aw3YCibADlwrVaAqIoXK8bcBNys4FxezkcKfhZt.jpeg",
+      "alt": "Komisi Dibagikan Setiap Hari"
+    }
+  ]
+}
