45512e4b467b65c83023264e48dcb3bfa9d1abb6
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-12 20:01:00 +0700

    Create q11bet-base.js

---
+++ q11bet-base.js
@@ -0,0 +1,32 @@
+{
+	"items": [
+		{
+					"url": "https://i.imgur.com/Xvw56RJ.jpg",
+					"alt": "Menerima Deposit Via Pulsa dan Emoney"
+		},
+		{
+					"url": "https://i.imgur.com/wFiUp2g.jpg",
+					"alt": "Promo PSBB"
+        },
+		{
+					"url": "https://media.fastchecker.us/idnsmedia/is/slots-v3/promotions/desktop/psdesktop.webp",
+					"alt": ""
+        },
+        {
+					"url": "https://media.fastchecker.us/idnsmedia/is/slots-v3/promotions/desktop/ttgdesktgop45.webp",
+					"alt": ""
+        },
+        {
+					"url": "https://media.fastchecker.us/idnsmedia/is/slots-v3/promotions/desktop/pgbionusszxiladeskto.webp",
+					"alt": ""
+        },
+        {
+					"url": "https://media.fastchecker.us/idnsmedia/is/slots-v3/promotions/desktop/princessdesktop.webp",
+					"alt": ""
+        },
+        {
+					"url": "https://media.fastchecker.us/idnsmedia/is/slots-v3/promotions/desktop/idnslotdesktop31.webp",
+					"alt": ""
+        }
+	]
+}
