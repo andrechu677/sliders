8148daecb367b6684fb9f712521adba04dd55632
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-12 21:18:56 +0700

    Create qqgalaxy-base.js

---
+++ qqgalaxy-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://139.162.7.78/Content/Web/common/images/catch/9691b45bccac48ba9f218b091e9b0cb6.jpg",
+					"alt": "Promo PSBB"
+		},
+		{
+					"url": "https://139.162.7.78/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
+					"alt": "Komisi Mingguan Terbesar"
+        },
+		{
+					"url": "https://139.162.7.78/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
+					"alt": "Proses Deposit Cepat"
+        }
+	]
+}
