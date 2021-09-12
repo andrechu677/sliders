088d6584c993ae5adaf0332cc2bf042438036f2d
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-12 21:56:24 +0700

    Create slotace-base.js

---
+++ slotace-base.js
@@ -0,0 +1,20 @@
+{
+	"items": [
+		{
+					"url": "https://52.77.164.45/Content/Web/common/images/catch/f9d41e031f694742814e69201531332d.jpg",
+					"alt": "Promo PSBB"
+		},
+		{
+					"url": "https://52.77.164.45/Content/Web/common/images/catch/404c822a5588476f9d276746fbcdb6ab.jpg",
+					"alt": "Komisi Game dibagikan setiap minggu"
+        },
+		{
+					"url": "https://52.77.164.45/Content/Web/common/images/catch/4a2f100a78fb49d3a59fe241208e60f0.jpg",
+					"alt": "Welcome Bonus"
+        },
+        {
+					"url": "https://52.77.164.45/Content/Web/common/images/catch/43cfafb990914decae4deea72c966b0e.jpg",
+					"alt": "Langkah deposit mudah dan cepat"
+        }
+	]
+}
