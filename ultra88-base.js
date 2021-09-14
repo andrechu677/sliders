3cc5a11d6ea1a8ada05875c04bdafe2e713dcd60
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-14 23:57:22 +0700

    Create ultra88-base.js

---
+++ ultra88-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/96m0T5NAXIDkRTUW1PeClM8MlzniQ9iHjZYO5UvJ.jpeg",
+      "alt": "Promo PSBB"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/wApy3g1ku6Hb8xtH5iXa2hONB9giwXDX1Z41Arbl.jpeg",
+      "alt": "Deposit Via Pulsa"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/1591098840_slider-teknik-deposit.jpg",
+      "alt": "Ultra88 didukung dengan Sistem Deposit Tercepat"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/1591098854_welcome-bonus.jpg",
+      "alt": "Welcome Bonus Slot 100%"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/14kd48f70VxY5yWNIIegD3Xk05jyEKd48C3DDi94.jpeg",
+      "alt": "Komisi Dibagikan Setiap Hari"
+    }
+  ]
+}
