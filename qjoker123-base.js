044224ed0e5a0f480bb19aef889dccdf22cefde8
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-09-24 04:11:55 +0700

    Create qjoker123-base.js

---
+++ qjoker123-base.js
@@ -0,0 +1,24 @@
+{
+	"items": [
+		{
+					"url": "https://i.imgur.com/LI5uU4P.jpg",
+					"alt": "PROMO PSBB"
+		},
+		{
+					"url": "https://i.imgur.com/3LowQVi.jpg",
+					"alt": "WELCOME BONUS 20%"
+        },
+		{
+					"url": "https://i.imgur.com/d0gH6Ly.jpg",
+					"alt": "MENERIMA DEPOSIT VIA PULSA DAN EMONEY"
+        },
+        {
+					"url": "https://i.imgur.com/YqjKLGB.jpg",
+					"alt": "MENGGUNAKAN SISTEM AUTO DEPOSIT"
+        },
+        {
+					"url": "https://i.imgur.com/z8vsFrV.jpg",
+					"alt": "BONUS MEMBER DIBAGIKAN SETIAP MINGGU"
+        }
+	]
+}
