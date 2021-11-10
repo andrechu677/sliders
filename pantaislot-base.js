fd8a77d094bc211c2f02d233a7c950197d9be426
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-11-10 12:25:59 +0700

    Create pantaislot-base.js

---
+++ pantaislot-base.js
@@ -0,0 +1,28 @@
+{
+  "items": [
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner1.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner2.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner3.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner4.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner5.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner6.jpg",
+      "alt": ""
+    }
+  ]
+}
