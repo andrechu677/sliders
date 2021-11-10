29746114ca65df645823cb25be5e486a805e0ce9
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-11-10 12:26:28 +0700

    Create wangi4d-base.js

---
+++ wangi4d-base.js
@@ -0,0 +1,20 @@
+{
+  "items": [
+    {
+      "url": "https://silverliningplus.com/wp-content/uploads/2021/10/banner1.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://silverliningplus.com/wp-content/uploads/2021/10/banner2.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://silverliningplus.com/wp-content/uploads/2021/10/banner3.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://silverliningplus.com/wp-content/uploads/2021/10/banner4.jpg",
+      "alt": ""
+    }
+  ]
+}
