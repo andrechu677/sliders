2881afd078936dde830d1b6c35a1858a3415ab47
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-12-24 17:53:09 +0700

    Update and rename totoking-base.js to totoking4d-base.js

---
+++ totoking4d-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "http://206.189.83.64/wp-content/uploads/2021/12/banner1.jpg",
+      "alt": ""
+    },
+    {
+      "url": "http://206.189.83.64/wp-content/uploads/2021/12/banner2.jpg",
+      "alt": ""
+    },
+    {
+      "url": "http://206.189.83.64/wp-content/uploads/2021/12/banner3.jpg",
+      "alt": ""
+    },
+    {
+      "url": "http://206.189.83.64/wp-content/uploads/2021/12/banner4.jpg",
+      "alt": ""
+    },
+    {
+      "url": "http://206.189.83.64/wp-content/uploads/2021/12/banner5.jpg",
+      "alt": ""
+    }
+  ]
+}
