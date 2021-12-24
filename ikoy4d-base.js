b36c60f55b26c7d7649e570105dfae1dccac550e
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-12-24 11:45:02 +0700

    Create ikoy4d-base.js

---
+++ ikoy4d-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "https://139.59.30.151/images/upload-Slides-20211204171916.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://139.59.30.151/images/upload-Slides-20210913200824.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://139.59.30.151/images/upload-Slides-20210913200809.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://img.pay4d.info/banner/banner-piggypp-okt.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://img.pay4d.info/banner/slide-haba-desc.jpg",
+      "alt": ""
+    }
+  ]
+}
