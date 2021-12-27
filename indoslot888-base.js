ccdee9579c7734001653890b131682d38334216f
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2021-12-27 12:40:20 +0700

    Create indoslot888-base.js

---
+++ indoslot888-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "https://128.199.92.64/wp-content/themes/amp88/assets/images/banner1.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://128.199.92.64/wp-content/themes/amp88/assets/images/banner2.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://128.199.92.64/wp-content/themes/amp88/assets/images/banner3.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://128.199.92.64/wp-content/themes/amp88/assets/images/banner4.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://128.199.92.64/wp-content/themes/amp88/assets/images/banner5.jpg",
+      "alt": ""
+    }
+  ]
+}
