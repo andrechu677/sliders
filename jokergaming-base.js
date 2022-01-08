f124bdbe22b5f442c252ee83c982d5b22c8bf931
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-08 22:05:54 +0700

    Create jokergaming-base.js

---
+++ jokergaming-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://i.imgur.com/fpujEi0.png",
+					"alt": "Grand Jackpoy"
+		},
+		{
+					"url": "https://i.imgur.com/gV8iJWu.png",
+					"alt": "Free Spin for Real Money"
+        },
+		{
+					"url": "https://i.imgur.com/jzTrZhz.png",
+					"alt": "Power Play"
+        }
+	]
+}
