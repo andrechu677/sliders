15078bec3e69ab5a6f08216b1db0f470817c20f1
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-08 22:03:06 +0700

    Create joker123-base.js

---
+++ joker123-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://imgur.com/fpujEi0",
+					"alt": "Grand Jackpoy"
+		},
+		{
+					"url": "https://imgur.com/gV8iJWu",
+					"alt": "Free Spin for Real Money"
+        },
+		{
+					"url": "https://imgur.com/jzTrZhz,
+					"alt": "Power Play"
+        }
+	]
+}
