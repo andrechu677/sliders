6286ae92c6931aa139dbf493b864834d88bce726
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-14 20:11:43 +0700

    Update pantaislot-base.js

--- pantaislot-base.js
+++ pantaislot-base.js
@@ -1,27 +1,27 @@
 {
   "items": [
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner1.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner1.jpg",
       "alt": ""
     },
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner2.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner2.jpg",
       "alt": ""
     },
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner3.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner3.jpg",
       "alt": ""
     },
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner4.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner4.jpg",
       "alt": ""
     },
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner5.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner5.jpg",
       "alt": ""
     },
     {
-      "url": "https://mkredyty.net/wp-content/uploads/2021/10/banner6.jpg",
+      "url": "http://185.39.137.110/wp-content/uploads/2021/10/banner6.jpg",
       "alt": ""
     }
   ]
