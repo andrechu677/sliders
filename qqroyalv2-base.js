78791922cccafbad2808c9084fc7b72b04053ff3
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-28 00:16:44 +0700

    Create qqroyalv2-base.js

---
+++ qqroyalv2-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://art-elka.com/wp-content/uploads/2022/01/62f7342dc2c64db5a54f1618b37bd59f.jpg",
+					"alt": "Promo Slot Berhadiah Uang Tunai"
+		},
+		{
+					"url": "https://art-elka.com/wp-content/uploads/2022/01/a2019a292ae148d78dc96ad3020c1a78.jpg",
+					"alt": "Komisi Mingguan"
+        },
+		{
+					"url": "https://art-elka.com/wp-content/uploads/2022/01/790428118a0942fbb879fbed592b9f62.jpg",
+					"alt": "Live Game Resmi"
+        }
+	]
+}
