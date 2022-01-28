47d6404ef205face6d31af933b0be4f4dce4940d
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-28 17:17:06 +0700

    Create asiabet118-base.js

---
+++ asiabet118-base.js
@@ -0,0 +1,28 @@
+{
+	"items": [
+		{
+					"url": "https://files.sitestatic.net/banners/pLCfLqFZDhZKW7aGBl5hSABcKqnqWL0co73sPjuL.jpeg",
+					"alt": ""
+		},
+		{
+					"url": "https://files.sitestatic.net/banners/2CIWQj0yvXaMT4MaLxA1jLG8GU4U1L4Gd18ExW4A.jpeg",
+					"alt": ""
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/ViHPtvR96MxVRXpb4kEn0IciflUeh4VApy6krXrb.jpeg",
+					"alt": ""
+        },
+        {
+					"url": "https://files.sitestatic.net/banners/1584703157_SLIDER-2.jpg",
+					"alt": ""
+        },
+        {
+					"url": "https://files.sitestatic.net/banners/1584703151_SLIDER-3.jpg",
+					"alt": ""
+        },
+        {
+					"url": "https://files.sitestatic.net/banners/RhyjiSWpvVKRrNu1SiNLEnVhWnSm4hdXTqkBPzCe.jpeg",
+					"alt": ""
+        }
+	]
+}
