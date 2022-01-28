eab405865899c9bb648167ece4d9e820c5ba9541
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-01-28 18:16:11 +0700

    Create qqomega-base.js

---
+++ qqomega-base.js
@@ -0,0 +1,20 @@
+{
+  "items": [
+    {
+      "url": "https://3.1.251.186/Content/Web/common/images/catch/9f7ff5f989374a1d9f206364ddbbe199.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://3.1.251.186/Content/Web/common/images/catch/a91df8952ced49d098b779b88eafa6d5.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://3.1.251.186/Content/Web/common/images/catch/491d378bd9e341c084cf6e867b426b22.jpg",
+      "alt": ""
+    },
+    {
+      "url": "https://3.1.251.186/Content/Web/common/images/catch/6260f2025b244e2cb0f3e4853908f0bb.jpg",
+      "alt": ""
+    }
+  ]
+}
