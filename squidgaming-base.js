5e744687e22dcc5260ca0b1e33a7bd465c5312e5
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-02-10 22:28:13 +0700

    Create squidgaming-base.js

---
+++ squidgaming-base.js
@@ -0,0 +1,23 @@
+{
+	"items": [
+		{
+					"url": "https://files.sitestatic.net/banners/61fe73da7fa13_slider.jpg",
+					"alt": "Cuan Februari"
+		},
+		{
+					"url": "https://files.sitestatic.net/banners/61652035434fe_1.jpg",
+					"alt": "Bonus Slot 100%"
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/6165206dee4d5_2.jpg",
+					"alt": "Deposit Ewallet dan Pulsa"
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/61652098de661_3.jpg",
+					"alt": "Sistem Deposit otomatis dan tercepat"
+		},
+    { "url": "https://files.sitestatic.net/banners/616520aac4960_4.jpg",
+					"alt": "Komisi dibagikan setiap hari"
+    }
+	]
+}
