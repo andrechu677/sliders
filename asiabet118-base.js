072b7009a977b555d45ab720d13b82cc323cfd4b
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-03-07 19:41:45 +0700

    Update asiabet118-base.js

--- asiabet118-base.js
+++ asiabet118-base.js
@@ -2,27 +2,27 @@
 	"items": [
 		{
 					"url": "https://files.sitestatic.net/banners/pLCfLqFZDhZKW7aGBl5hSABcKqnqWL0co73sPjuL.jpeg",
-					"alt": ""
+					"alt": "All jackpot prize 100%"
 		},
 		{
-					"url": "https://files.sitestatic.net/banners/2CIWQj0yvXaMT4MaLxA1jLG8GU4U1L4Gd18ExW4A.jpeg",
-					"alt": ""
+					"url": "https://files.sitestatic.net/banners/ZJWMV7pdsyOjc7nTevkCAyqoFLfjWMVCmyCh4fOc.jpeg",
+					"alt": "Promo bulan meriah (Mater melimpah)"
         },
 		{
 					"url": "https://files.sitestatic.net/banners/ViHPtvR96MxVRXpb4kEn0IciflUeh4VApy6krXrb.jpeg",
-					"alt": ""
+					"alt": "Menerima deposit via pulsa"
         },
         {
 					"url": "https://files.sitestatic.net/banners/1584703157_SLIDER-2.jpg",
-					"alt": ""
+					"alt": "Welcome bonus slot"
         },
         {
 					"url": "https://files.sitestatic.net/banners/1584703151_SLIDER-3.jpg",
-					"alt": ""
+					"alt": "Sistem dukungan auto deposit"
         },
         {
 					"url": "https://files.sitestatic.net/banners/RhyjiSWpvVKRrNu1SiNLEnVhWnSm4hdXTqkBPzCe.jpeg",
-					"alt": ""
+					"alt": "Komisi dibagikan setiap hari"
         }
 	]
 }
