fd46f5a6b4103a1c9cd4fb01a827063fdbbec0f3
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-04-09 00:06:05 +0700

    Update qqgalaxy-base.js

--- qqgalaxy-base.js
+++ qqgalaxy-base.js
@@ -1,15 +1,15 @@
 {
 	"items": [
 		{
-					"url": "https://139.162.7.78/Content/Web/common/images/catch/9691b45bccac48ba9f218b091e9b0cb6.jpg",
-					"alt": "Promo PSBB"
+					"url": "https://95.111.195.144/Content/Web/common/images/catch/7af6daf76b79488fafa2cdb51ca7f11a.jpg",
+					"alt": "Promo Bulan April"
 		},
 		{
-					"url": "https://139.162.7.78/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
+					"url": "https://95.111.195.144/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
 					"alt": "Komisi Mingguan Terbesar"
         },
 		{
-					"url": "https://139.162.7.78/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
+					"url": "https://95.111.195.144/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
 					"alt": "Proses Deposit Cepat"
         }
 	]
