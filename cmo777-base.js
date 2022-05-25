ed1541e308727746b1685d91f1b1599d5903fd9d
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-05-26 03:10:44 +0700

    Create cmo777-base.js

---
+++ cmo777-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "http://cmo777.com/images/upload-Slides-20220227171929.jpg",
+					"alt": "Deposit mulai dari sepuluh ribu"
+		},
+		{
+					"url": "http://cmo777.com/images/upload-Slides-20220227171946.jpg",
+					"alt": "Promo bonus khusus semua member"
+        },
+		{
+					"url": "http://cmo777.com/images/upload-Slides-20220227171956.jpg",
+					"alt": "Bonus rolling slot dan tembak ikan"
+        }
+	]
+}
