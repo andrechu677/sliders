0136343bf81d6ea0c70df681651efff9ca3e2102
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-05-30 14:00:26 +0700

    Create ratuking4d-base.js

---
+++ ratuking4d-base.js
@@ -0,0 +1,28 @@
+{
+	"items": [
+		{
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/ratuking-mei_20220843130843.jpg",
+					"alt": "Promo Ameizing"
+		},
+		{
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi-slot_20211255141255.jpg",
+					"alt": "Komisi mingguan Slot Game"
+        },
+		{
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi-casino_20211326141326.jpg",
+					"alt": "Komisi Mingguan Live Casino"
+        },
+        {
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi%20sportsbook_20211357141357.jpg",
+					"alt": "Komisi Mingguan Sportsnook"
+        },
+        {
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/cashback-dingdong-ayam-ikan_20211441141441.jpg",
+					"alt": "Bonus Cashback Dingdong Sabung Ayam Tembak Ikan"
+        },
+        {
+					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/ratuking_20215636115636.jpg",
+					"alt": "Menerima deposit via pulsa dan ewallet"
+        }
+	]
+}
