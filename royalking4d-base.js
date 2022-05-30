6803bad152deb193ee4408f1d51f3325cad743bc
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-05-30 13:57:49 +0700

    Create royalking4d-base.js

---
+++ royalking4d-base.js
@@ -0,0 +1,28 @@
+{
+	"items": [
+		{
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/royalking-mei_20221254131254.jpg",
+					"alt": "Promo Ameizing"
+		},
+		{
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/komisi-mingguan-slot_20210644080644.jpg",
+					"alt": "Komisi mingguan Slot Game"
+        },
+		{
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/komisi-mingguan-casino_20210708080708.jpg",
+					"alt": "Komisi Mingguan Live Casino"
+        },
+        {
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/komisi-sportsbook_20210733080733.jpg",
+					"alt": "Komisi Mingguan Sportsnook"
+        },
+        {
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/cashback-togel_20210816080816.jpg",
+					"alt": "Bonus Cashback Togel"
+        },
+        {
+					"url": "https://royalking.xn--6frz82g/Assets/banner/IND/royalking4d_20215947115947.jpg",
+					"alt": "Menerima deposit via pulsa dan ewallet"
+        }
+	]
+}
