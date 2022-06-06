3815394452368f240d00c53a2d3c4f0e82cef066
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-06-06 23:04:04 +0700

    Update qqgalaxy-base.js

--- qqgalaxy-base.js
+++ qqgalaxy-base.js
@@ -1,15 +1,15 @@
 {
 	"items": [
 		{
-					"url": "https://95.111.195.144/Content/Web/common/images/catch/7af6daf76b79488fafa2cdb51ca7f11a.jpg",
-					"alt": "Promo Bulan April"
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/cd18b3b216d54a8ab9f31eb60dda56c1.jpg",
+					"alt": "Promo Jumbo, Juni Maksimal Bonus"
 		},
 		{
-					"url": "https://95.111.195.144/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
 					"alt": "Komisi Mingguan Terbesar"
         },
 		{
-					"url": "https://95.111.195.144/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
 					"alt": "Proses Deposit Cepat"
         }
 	]
