a47079b70e9a5213da18f5065d4dd9ebb75d75d0
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-06-07 22:49:17 +0700

    Update qqroyales-base.js

--- qqroyales-base.js
+++ qqroyales-base.js
@@ -1,15 +1,15 @@
 {
 	"items": [
 		{
-					"url": "https://95.111.201.215:30000/Content/Web/common/images/catch/41d369f824db41539810b9d60e74c28b.jpg",
-					"alt": "Promo Ameizing Melimpah hadiah yang meriah"
+					"url": "https://95.111.203.62:30000/Content/Web/common/images/catch/29d7d3afd1364bac89cb533a7bc6b1be.jpg",
+					"alt": "Promo Jumbo Juni Maksimal Bonus"
 		},
 		{
-					"url": "https://95.111.201.215:30000/Content/Web/common/images/catch/a2019a292ae148d78dc96ad3020c1a78.jpg",
+					"url": "https://95.111.203.62:30000/Content/Web/common/images/catch/a2019a292ae148d78dc96ad3020c1a78.jpg",
 					"alt": "Rebate Komisi Mingguan"
         },
 		{
-					"url": "https://95.111.201.215:30000/Content/Web/common/images/catch/790428118a0942fbb879fbed592b9f62.jpg",
+					"url": "https://95.111.203.62:30000/Content/Web/common/images/catch/790428118a0942fbb879fbed592b9f62.jpg",
 					"alt": "Komisi setiap minggu dari qqroyal. Paling banyak bonusnya"
         }
 	]
