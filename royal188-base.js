dade5b5c8de7407b8a296130c8f8de1528b40a3d
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-08-07 03:22:23 +0700

    Update royal188-base.js

--- royal188-base.js
+++ royal188-base.js
@@ -1,28 +1,16 @@
 {
   "items": [
     {
-      "url": "https://files.sitestatic.net/banners/n5xd7P5kBV1RIlqFNc961aH0P5eGXx7WYo044rwq.jpg",
-      "alt": "Grand Proze Slot"
+      "url": "https://files.sitestatic.net/banners/438ZKtskUDd9v8bA3tKCDEHltiWGlQgC6fjq2bxX.jpg",
+      "alt": "Promo Merdeka"
     },
     {
-      "url": "https://files.sitestatic.net/banners/YEJ9ws3Ps0RnDmADlVjGg995zMQglrTYDrHuXohO.jpg",
-      "alt": "Maret Berlimpah Hadiah"
+      "url": "https://files.sitestatic.net/banners/K6RXkTP6v1zLLZsbFFfNLHvrbbjcBtmk94B4wrYm.jpg",
+      "alt": "Welcome bonus slot 100%"
     },
     {
-      "url": "https://files.sitestatic.net/banners/C88d7kM6DcDszaI4LuYglwfqTdVv7jiVQxEZ3AA7.jpg",
-      "alt": "Menerima deposit via pulsa telkomsel xl dan emoney"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/1610536048_SISTEM-AUTO-DEPOSIT.jpg",
-      "alt": "Didukung oleh sistem auto deposit tercepat"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/1610536076_WELCOME-BONUS-SLOT.jpg",
-      "alt": "Bonus slot member baru"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/zCO2G6rPUz2Ghrw81fjZrQd9sQtrhoMhOa4KHS22.jpg",
-      "alt": "Komisi dibagikan setiap hari"
+      "url": "https://files.sitestatic.net/banners/dsdZ83MQl71pSMnAfFXJDD4jZ7cDCBKzZtBmoRCI.jpg",
+      "alt": "Didukung oleh sistem auto deposit"
     }
   ]
 }
