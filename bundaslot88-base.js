e2c59e4129a400ecd2045db90349867c024b4cba
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-08-12 16:46:44 +0700

    Create bundaslot88-base.js

---
+++ bundaslot88-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://api2-bun.tr8ngames.com/images/id_cbm_c503dda5-3c82-4569-bfd5-74ff0ce51e67_1658887634247.png",
+					"alt": "Selamat datang di Bundaslot88"
+		},
+		{
+					"url": "https://api2-bun.tr8ngames.com/images/id_cbm_6ebeb326-735a-4e09-bfe3-d17a9e914e36_1658907608640.png",
+					"alt": "Minimal depo dan wd"
+        },
+		{
+					"url": "https://api2-bun.tr8ngames.com/images/id_cbm_9fc07d1d-ca5f-41f8-8464-a8861cf66efc_1658890086150.png",
+					"alt": "Promo Bonus dan kontak bundaslot"
+        }
+	]
+}
