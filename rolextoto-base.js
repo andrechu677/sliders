2e0a0220d943772da819246d1da0532be4622e4a
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-09-03 14:55:47 +0700

    Create rolextoto-base.js

---
+++ rolextoto-base.js
@@ -0,0 +1,24 @@
+{
+  "items": [
+    {
+      "url": "https://www.totorolexac.org/images/upload-Slides-20220805140946.jpg",
+      "alt": "Promo Kemerdekaan"
+    },
+    {
+      "url": "https://www.totorolexac.org/images/upload-Slides-20220308170827.jpg",
+      "alt": "Menerima deposit pulsa dan ewallet"
+    },
+    {
+      "url": "https://img.pay4d.info/banner/banner-piggypp-okt.jpg",
+      "alt": "Daily Wins Pragmatic Play"
+    },
+    {
+      "url": "https://img.pay4d.info/banner/hbmg.jpg",
+      "alt": "Eksklusif Habanero"
+    },
+    {
+      "url": "https://img.pay4d.info/banner/sg-mar.jpg",
+      "alt": "Turnamen Musim Panas"
+    }
+  ]
+}
