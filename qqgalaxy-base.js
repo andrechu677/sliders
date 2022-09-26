465e5a20292f36e640839cb4d29e17ba7b45defc
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-09-26 19:55:39 +0700

    Update qqgalaxy-base.js

--- qqgalaxy-base.js
+++ qqgalaxy-base.js
@@ -1,15 +1,15 @@
 {
 	"items": [
 		{
-					"url": "https://qqgalaxyto.com/Content/Web/common/images/catch/0d3e4308aacd4e26ad5a9e144d46c02e.jpg",
-					"alt": "Promo Merdeka"
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/b3f43bc667a741d0bf54114fc05dfae1.jpg",
+					"alt": "Promo Spektakuler"
 		},
 		{
-					"url": "https://qqgalaxyto.com/Content/Web/common/images/catch/647b6da080d64d708df2e80c4c6f5f80.jpg",
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/0d8f1298ee604a37870823799cca8483.jpg",
 					"alt": "Komisi Mingguan Terbesar"
         },
 		{
-					"url": "https://qqgalaxyto.com/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
+					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
 					"alt": "Proses Deposit Cepat"
         }
 	]
