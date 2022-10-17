e67f440f8aa8319c171b3a1c56d923287d1a21aa
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-10-17 14:19:47 +0700

    Update tahun88-base.js

--- tahun88-base.js
+++ tahun88-base.js
@@ -1,16 +1,16 @@
 {
 	"items": [
 		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbm_ab151be8-9a06-4ac9-a954-bb6b05a0c29d_1658988415330.png",
+					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_ab151be8-9a06-4ac9-a954-bb6b05a0c29d_1663942259810.jpg",
 					"alt": "Selamat datang di tahun88"
 		},
 		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbm_63b6dd26-5457-4a4c-8645-1a14b4922fce_1658988422847.png",
-					"alt": "Minimal depo dan withdraw"
+					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_63b6dd26-5457-4a4c-8645-1a14b4922fce_1663943206150.jpg",
+					"alt": "Transaksi mudah dan cepat"
         },
 		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbm_96792d66-e28e-43fb-b7cb-b9e4abf4abe2_1658988338173.png",
-					"alt": "Promo Bonus"
+					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_96792d66-e28e-43fb-b7cb-b9e4abf4abe2_1663943296300.jpg",
+					"alt": "Promo Slot Menang kalah Dapet Duit"
         }
 	]
 }
