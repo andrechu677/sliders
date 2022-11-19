bf8612ffdfb42d7b86faf0bb75c8d6c666dd6e69
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-19 23:23:23 +0700

    Create kpk138-base.js

---
+++ kpk138-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://nx-cdn.trgwl.com/Images/banners/home/pp-spin-and-score-desktop.png",
+      "alt": ""
+    },
+    {
+      "url": "https://nx-cdn.trgwl.com/Images/banners/home/mg-million-cash-splash-desktop.png",
+      "alt": ""
+    },
+    {
+      "url": "https://nx-cdn.trgwl.com/Images/banners/home/release-the-kraken-pp-desktop.png",
+      "alt": ""
+    }
+  ]
+}
