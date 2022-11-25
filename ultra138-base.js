082216518f508d20851ba099e28ecefd8b45fce9
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-26 04:47:31 +0700

    Create ultra138-base.js

---
+++ ultra138-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/6364d4420214d_ultra138.jpg",
+      "alt": "Promo November"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/61fa7cdb5ac40_1.png",
+      "alt": "Menerima deposit via pulsa"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/62eb9e2509b36_welcome.png",
+      "alt": "Bonus slot 100%"
+    }
+  ]
+}
