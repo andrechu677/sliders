b52a06847667065dba8eee1138e0f340ef6dfcd7
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-26 05:33:56 +0700

    Create ultra777-base.js

---
+++ ultra777-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/6364d050d9feb_ULTRA777.jpg",
+      "alt": "Promo November"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/61fa77d550ab3_1.png",
+      "alt": "Menerima deposit via pulsa"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/62eb9f690caba_welcome.png",
+      "alt": "Welcome Bonus slot 100%"
+    }
+  ]
+}
