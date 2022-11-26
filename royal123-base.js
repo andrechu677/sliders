f730d4237e7c353fe24c5add51db29f171178084
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-26 09:44:28 +0700

    Create royal123-base.js

---
+++ royal123-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://files.sitestatic.net/banners/637c59c364099_royal123.jpg",
+					"alt": "Fifa Worldcup Qatar 2022"
+		},
+		{
+					"url": "https://files.sitestatic.net/banners/6364f91f2fb68_royal123.jpg",
+					"alt": "Promo Novemberia"
+        },
+		{
+					"url": "https://files.sitestatic.net/banners/62a6c4a61c601_3.jpg",
+					"alt": "Menerima deposit via pulsa, ewallet dan transfer bank"
+        }
+	]
+}
