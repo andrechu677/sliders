560bfc1b9d544e244622ec0dbde23769e9cfb5ff
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-26 10:27:04 +0700

    Create ultrajp-base.js

---
+++ ultrajp-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/6364cb4e1a27f_ultrajp.jpg",
+      "alt": "Promo November hingga 100juta"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/6374d5fc6ad59_ultrajp.jpg",
+      "alt": "Fifa Worldcup 2022 Qatar"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/62a7087be6060_14%20(1).jpg",
+      "alt": "Swposit Pulsa ewallet dan bank transfer"
+    }
+  ]
+}
