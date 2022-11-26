bc01588ce3ce46ee3de3d7c64bf71b94fe4be557
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-11-26 07:02:35 +0700

    Update royal138-base.js

--- royal138-base.js
+++ royal138-base.js
@@ -1,13 +1,14 @@
 {
 	"items": [
 		{
+					"url": "https://files.sitestatic.net/banners/6374d4d53667d_royal;138.jpg",
+					"alt": "Fifa Worldcup 2022"
+        },
+		{
 					"url": "https://files.sitestatic.net/banners/6364ade378310_royal138.jpg",
 					"alt": "Promo November"
 		},
-		{
-					"url": "https://files.sitestatic.net/banners/62dbdc9eec90d_WEB-SLIDER.jpg",
-					"alt": "Komisi dibagikan setiap hari"
-        },
+		
 		{
 					"url": "https://files.sitestatic.net/banners/62eb982cbe369_slider-welcome.jpg",
 					"alt": "Welcome bonus solot 100%"
