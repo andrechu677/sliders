152358cce7ccdb638b5891036dd53e8d9255d285
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-12-02 23:14:30 +0700

    Create boneka88-base.js

---
+++ boneka88-base.js
@@ -0,0 +1,20 @@
+{
+  "items": [
+    {
+      "url": "https://sgacdn.azureedge.net/agent-websites/27/medialibrary/images/27_be9fa66a4f3d4cb4a0627e1fc8655339.jpg",
+      "alt": "Situs official resmi dan terpercaya betting piala dunia qatar"
+    },
+    {
+      "url": "https://sgacdn.azureedge.net/agent-websites/27/medialibrary/images/27_c1185602f4dd482ebc159822ac185b8e.jpg",
+      "alt": "Situs permainan online terbaik boneka88"
+    },
+    {
+      "url": "https://sgacdn.azureedge.net/agent-websites/27/medialibrary/images/27_7f9f64d3ea89489f82c92c906c93cb84.jpg",
+      "alt": "spin score cash drop"
+    },
+    {
+      "url": "https://sgacdn.azureedge.net/agent-websites/27/medialibrary/images/27_ce1f1fddbc2f4be486474709666157c1.png",
+      "alt": "menang setiap hari bermain slot & live casino"
+    }
+  ]
+}
