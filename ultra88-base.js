7cdf0b6035ebf6a66811159be2cb4f2346b42885
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-12-11 13:59:25 +0700

    Update ultra88-base.js

--- ultra88-base.js
+++ ultra88-base.js
@@ -1,24 +1,12 @@
 {
   "items": [
     {
-      "url": "https://files.sitestatic.net/banners/xim1OUY2PtIjedhAIPAgjCJkYwsIadORaJirLHpU.jpeg",
-      "alt": "Jackpot Permainan Joker Gaming"
+      "url": "https://files.sitestatic.net/banners/YN5KuZZzTTa1XU4PYSCpLSVZdSmiPE1RvrDMpbXG.jpeg",
+      "alt": "Fifa Qatar 2022"
     },
     {
-      "url": "https://files.sitestatic.net/banners/wApy3g1ku6Hb8xtH5iXa2hONB9giwXDX1Z41Arbl.jpeg",
-      "alt": "Deposit Via Pulsa"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/1591098840_slider-teknik-deposit.jpg",
-      "alt": "Ultra88 didukung dengan Sistem Deposit Tercepat"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/1591098854_welcome-bonus.jpg",
-      "alt": "Welcome Bonus Slot 100%"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/14kd48f70VxY5yWNIIegD3Xk05jyEKd48C3DDi94.jpeg",
-      "alt": "Komisi Dibagikan Setiap Hari"
+      "url": "https://files.sitestatic.net/banners/0D7OuzSVFrCmykT27JdvHPIUEbpmgo30A8URgX4O.jpeg",
+      "alt": "Welcome bonus 100%"
     }
   ]
 }
