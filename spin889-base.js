765322498f1e01aa948b634629f7dfbbf60f5a9c
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2022-12-20 00:38:12 +0700

    Create spin889-base.js

---
+++ spin889-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://api2-spp.tr8ngames.com/images/id_cbd_377f138c-8f8d-4195-a0a9-a209c1a3f0a4_1669162610017.jpg",
+					"alt": "Welcome bonus new member 50%"
+		},
+		{
+					"url": "https://nx-cdn.trgwl.com/Images/banners/home/ap-spin-and-win-desktop.jpg",
+					"alt": "Turnamen natal dan tahun baru"
+        },
+		{
+					"url": "https://api2-spp.tr8ngames.com/images/id_cbd_79bae5cc-191a-4fcb-aae9-5a913db26789_1669666327313.jpg",
+					"alt": "Event bonus free spin"
+        }
+	]
+}
