1969b0ed34a98c57fa9424fe754dd491d008128c
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-02-12 22:12:41 +0700

    Update ratuking4d-base.js

--- ratuking4d-base.js
+++ ratuking4d-base.js
@@ -1,28 +1,20 @@
 {
 	"items": [
 		{
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/ratuking-mei_20220843130843.jpg",
-					"alt": "Promo Ameizing"
+					"url": "https://www.ratukingco.com/assets/banner/IND/ratuking_20215636115636.jpg",
+					"alt": "Menerima depopsit via pulsa"
 		},
 		{
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi-slot_20211255141255.jpg",
-					"alt": "Komisi mingguan Slot Game"
+					"url": "https://www.ratukingco.com/assets/banner/IND/ratuking_20230018030018.jpg",
+					"alt": "Promo Februari Istimewa"
         },
 		{
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi-casino_20211326141326.jpg",
-					"alt": "Komisi Mingguan Live Casino"
+					"url": "https://www.ratukingco.com/assets/banner/IND/komisi-slot_20211255141255.jpg",
+					"alt": "Komisi Mingguan Slot Game"
         },
         {
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/komisi%20sportsbook_20211357141357.jpg",
-					"alt": "Komisi Mingguan Sportsnook"
-        },
-        {
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/cashback-dingdong-ayam-ikan_20211441141441.jpg",
-					"alt": "Bonus Cashback Dingdong Sabung Ayam Tembak Ikan"
-        },
-        {
-					"url": "https://ratuking.xn--6frz82g/Assets/banner/IND/ratuking_20215636115636.jpg",
-					"alt": "Menerima deposit via pulsa dan ewallet"
+					"url": "https://www.ratukingco.com/assets/banner/IND/cashback-dingdong-ayam-ikan_20211441141441.jpg",
+					"alt": "Bonus cashback mingguan"
         }
 	]
 }
