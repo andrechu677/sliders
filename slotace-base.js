65954138f4dce68324ade901b73cfa716f4b3327
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-02-12 22:16:28 +0700

    Update slotace-base.js

--- slotace-base.js
+++ slotace-base.js
@@ -1,20 +1,16 @@
 {
 	"items": [
 		{
-					"url": "https://slotace99cc.com/Content/Web/common/images/catch/3992de3000da4fc59f2bbe8ede6f2ada.jpg",
-					"alt": "Promo Merdeka"
+					"url": "https://slotace99cc.com/Content/Web/common/images/catch/7d562319516848e3a82fa7a92f16a680.jpg",
+					"alt": "Promo Februari Istimewa"
 		},
 		{
-					"url": "https://slotace99cc.com/Content/Web/common/images/catch/404c822a5588476f9d276746fbcdb6ab.jpg",
-					"alt": "Komisi Game dibagikan setiap minggu"
+					"url": "https://slotace99cc.com/Content/Web/common/images/catch/af28d5a9cc0b41899aff78bc770c5749.jpg",
+					"alt": "Komisi Mingguan terbesar"
         },
 		{
 					"url": "https://slotace99cc.com/Content/Web/common/images/catch/4a2f100a78fb49d3a59fe241208e60f0.jpg",
 					"alt": "Welcome Bonus"
-        },
-        {
-					"url": "https://slotace99cc.com/Content/Web/common/images/catch/43cfafb990914decae4deea72c966b0e.jpg",
-					"alt": "Langkah deposit mudah dan cepat"
         }
 	]
 }
