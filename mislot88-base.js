c65a683ccf2bc33bb1ba3eb34bb525b33f44d98a
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-02-19 21:03:11 +0700

    Create mislot88-base.js

---
+++ mislot88-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://api2-m8s.tr8n2games.com/images/id_cbd_91f1fbb6-c01b-42bc-b2be-21863a167888_1675673052100.jpg",
+					"alt": "FIESTA Promo Februari"
+		},
+		{
+					"url": "https://api2-m8s.tr8n2games.com/images/id_cbd_78e675df-61ae-4c11-bd24-ae3846c153be_1665125508260.jpg",
+					"alt": "Menerima deposit via pulsa bank transfer dan emoney"
+        },
+		{
+					"url": "https://api2-m8s.tr8n2games.com/images/id_cbd_e7b24b75-6399-49df-89f4-d60de1708eaf_1665125544263.jpg",
+					"alt": "Didukung oleh sistem auto deposit yang cepat dan handal"
+        }
+	]
+}
