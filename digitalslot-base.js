b5e9b07fe6632bd2b6e7b0109315faff764983ea
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-02-19 21:42:35 +0700

    Create digitalslot-base.js

---
+++ digitalslot-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://lbstatic.superdominator88.vip/media/images/7ODzG0jEUv6Le1IsoT7c.jpg",
+					"alt": "Promo Februari Istimewa"
+		},
+		{
+					"url": "https://lbstatic.superdominator88.vip/media/images/1fkDvSDk4rxnjsEnvIbw.jpg",
+					"alt": "Ada cashback mingguan"
+        },
+		{
+					"url": "https://lbstatic.superdominator88.vip/media/images/EaXyOJaW3WMkgMJu480E.jpg",
+					"alt": "Weekly cash rebate"
+        }
+	]
+}
