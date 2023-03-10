e71c48cdcf64bf1315d82febe6170477539f9589
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-03-11 00:49:19 +0700

    Update qqomega-base.js

--- qqomega-base.js
+++ qqomega-base.js
@@ -1,19 +1,19 @@
 {
   "items": [
     {
-      "url": "https://13.215.148.212:30000/Content/Web/common/images/catch/728858d14d2c46bf8c3d2f3a6b2e4865.jpg",
-      "alt": "Oktober makin gacor"
+      "url": "https://qqomegato.com/Content/Web/common/images/catch/a3cbe63c41bc4928948e8c51b9fca34a.jpg",
+      "alt": "MERIAH - Maret Berlimpah Hadiah"
     },
     {
-      "url": "https://13.215.148.212:30000/Content/Web/common/images/catch/a91df8952ced49d098b779b88eafa6d5.jpg",
+      "url": "https://qqomegato.com/Content/Web/common/images/catch/a91df8952ced49d098b779b88eafa6d5.jpg",
       "alt": "Welcome bonus 100%"
     },
     {
-      "url": "https://13.215.148.212:30000/Content/Web/common/images/catch/310f254f55a747bda2d05b6ba725d7e2.jpg",
+      "url": "https://qqomegato.com/Content/Web/common/images/catch/310f254f55a747bda2d05b6ba725d7e2.jpg",
       "alt": "Komisi mingguan terbesar"
     },
     {
-      "url": "https://13.215.148.212:30000/Content/Web/common/images/catch/6260f2025b244e2cb0f3e4853908f0bb.jpg",
+      "url": "https://qqomegato.com/Content/Web/common/images/catch/6260f2025b244e2cb0f3e4853908f0bb.jpg",
       "alt": "Didukung oleh sistem auto deposit"
     }
   ]
