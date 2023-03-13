3970480d9356fca6cd766f890491c27f40d988ba
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-03-13 18:44:36 +0700

    Update squidgaming-base.js

--- squidgaming-base.js
+++ squidgaming-base.js
@@ -1,16 +1,16 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/637c58ad7f519_slider-piala-duinia.jpg",
-					"alt": "Fifa Worldcup Piala Dunia Qatar"
+					"url": "https://files.sitestatic.net/banners/6402b304b5322_slider.jpg",
+					"alt": "Promo Maret Meriah"
 		},
 		{
 					"url": "https://files.sitestatic.net/banners/6165206dee4d5_2.jpg",
 					"alt": "Deposit pakai pulsa, ewallet dan bank transfer"
         },
 		{
-					"url": "https://files.sitestatic.net/banners/6364b0a7d65db_squidgaming.jpg",
-					"alt": "Promo November total hadiah puluhan juta"
+					"url": "https://files.sitestatic.net/banners/61652098de661_3.jpg",
+					"alt": "Dukungan sistem auto deposit"
         },
 		{
 					"url": "https://files.sitestatic.net/banners/62eb99a01f8fa_welcome.jpg",
