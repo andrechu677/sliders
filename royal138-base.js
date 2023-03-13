114508e0d8236f595f2814d8520334a26257d06a
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-03-13 19:15:04 +0700

    Update royal138-base.js

--- royal138-base.js
+++ royal138-base.js
@@ -1,18 +1,19 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/6374d4d53667d_royal;138.jpg",
-					"alt": "Fifa Worldcup 2022"
+					"url": "https://files.sitestatic.net/banners/6402b22d9aa41_ROYAL138.jpg",
+					"alt": "Hadiah Menarik di Bulan Maret"
         },
-		{
-					"url": "https://files.sitestatic.net/banners/6364ade378310_royal138.jpg",
-					"alt": "Promo November"
-		},
+		
 		
 		{
 					"url": "https://files.sitestatic.net/banners/62eb982cbe369_slider-welcome.jpg",
 					"alt": "Welcome bonus solot 100%"
         },
+		{
+					"url": "https://files.sitestatic.net/banners/63a90a6e40e69_5.jpg",
+					"alt": "Komisi dibagikan setiap hari"
+		},
 		{
 					"url": "https://files.sitestatic.net/banners/611ceb1d4c198_slider-bank.jpg",
 					"alt": "Didukung oleh sistem auto deposit"
