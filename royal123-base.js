6df28ef2fe09544b85e77b92ab5dc4f934437f68
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-03-13 18:21:20 +0700

    Update royal123-base.js

--- royal123-base.js
+++ royal123-base.js
@@ -1,12 +1,12 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/637c59c364099_royal123.jpg",
-					"alt": "Fifa Worldcup Qatar 2022"
+					"url": "https://files.sitestatic.net/banners/6402b62dde485_ROYAL123.jpg",
+					"alt": "Meriah 2023 - Maret Berlimpah Hadiah"
 		},
 		{
-					"url": "https://files.sitestatic.net/banners/6364f91f2fb68_royal123.jpg",
-					"alt": "Promo Novemberia"
+					"url": "https://files.sitestatic.net/banners/62eba05ec9b82_welcome.jpg",
+					"alt": "Welcome bonus slot 100%"
         },
 		{
 					"url": "https://files.sitestatic.net/banners/62a6c4a61c601_3.jpg",
