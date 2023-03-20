a3a379f978608fc18cd31fbee0816d5d7b144d46
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-03-21 01:39:24 +0700

    Update asiabet118-base.js

--- asiabet118-base.js
+++ asiabet118-base.js
@@ -1,27 +1,23 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/pLCfLqFZDhZKW7aGBl5hSABcKqnqWL0co73sPjuL.jpeg",
-					"alt": "All jackpot prize 100%"
+					"url": "https://files.sitestatic.net/banners/GHzfvyMGqm8DTpZAy2rK5xHY0DHzqiS1SYAVNNcc.jpeg",
+					"alt": "Promo maret melimpah hadiah"
 		},
 		{
-					"url": "https://files.sitestatic.net/banners/ZJWMV7pdsyOjc7nTevkCAyqoFLfjWMVCmyCh4fOc.jpeg",
-					"alt": "Promo bulan meriah (Mater melimpah)"
-        },
-		{
-					"url": "https://files.sitestatic.net/banners/ViHPtvR96MxVRXpb4kEn0IciflUeh4VApy6krXrb.jpeg",
+					"url": "https://files.sitestatic.net/banners/01kn80Hh81za1lbjUd6H9W6hIzGHjnMsIdBMKePJ.jpeg",
 					"alt": "Menerima deposit via pulsa"
         },
         {
-					"url": "https://files.sitestatic.net/banners/1584703157_SLIDER-2.jpg",
+					"url": "https://files.sitestatic.net/banners/aWiG8E0htzuwok1unfhnj05lJlz4zG19BVl489g7.jpeg",
 					"alt": "Welcome bonus slot"
         },
         {
-					"url": "https://files.sitestatic.net/banners/1584703151_SLIDER-3.jpg",
+					"url": "https://files.sitestatic.net/banners/U8BhIYtGx5Ho64SiuWzIaOpPEtUNbVDoeFOme0nY.jpeg",
 					"alt": "Sistem dukungan auto deposit"
         },
         {
-					"url": "https://files.sitestatic.net/banners/RhyjiSWpvVKRrNu1SiNLEnVhWnSm4hdXTqkBPzCe.jpeg",
+					"url": "https://files.sitestatic.net/banners/l5HQkq6nhJYNmQMIBkXQU2yd8AajCHSbwBecFmOC.jpeg",
 					"alt": "Komisi dibagikan setiap hari"
         }
 	]
