135978c196123cc0a920d592c89129df51de6f7b
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-04-19 02:46:06 +0700

    Update sga898-base.js

--- sga898-base.js
+++ sga898-base.js
@@ -1,7 +1,7 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/GHzfvyMGqm8DTpZAy2rK5xHY0DHzqiS1SYAVNNcc.jpeg",
+					"url": "https://sgacdn.azureedge.net/agent-websites/58/medialibrary/images/58_bcad71b095ce4924812a5c2d6a52f6ee.jpg",
 					"alt": "Welcome bonus new member 50% khusus slot game"
 		}
 	]
