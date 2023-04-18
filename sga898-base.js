85f819e9b8fa42370437e007e2541ccc65035b05
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-04-19 02:41:45 +0700

    Create sga898-base.js

---
+++ sga898-base.js
@@ -0,0 +1,8 @@
+{
+	"items": [
+		{
+					"url": "https://files.sitestatic.net/banners/GHzfvyMGqm8DTpZAy2rK5xHY0DHzqiS1SYAVNNcc.jpeg",
+					"alt": "Welcome bonus new member 50% khusus slot game"
+		}
+	]
+}
