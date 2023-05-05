5cf2bac8686969736c930f637cd62770b946b175
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-05 21:23:39 +0700

    Create alfabet188-base.js

---
+++ alfabet188-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/644f926f4e6d0_alfabet.jpg",
+      "alt": "Promo Mei Meriah Total Hadiah 90juta"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/62fa3e9f943c3_2.jpg",
+      "alt": "Komisi dibagikan setiap hari"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/62fa3a82c3746_4.jpg",
+      "alt": "Didukung oleh sistem deposit tercepat"
+    }
+  ]
+}
