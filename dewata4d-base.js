b7656dcbd32b8e355dfd2a00baaa2d4ef560310d
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-09 00:00:03 +0700

    Update dewata4d-base.js

--- dewata4d-base.js
+++ dewata4d-base.js
@@ -1,16 +1,16 @@
 {
   "items": [
     {
-      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_2c3c9e17-b791-4d6e-b7f8-33194d2d5f9e_1666952707650.jpg",
-      "alt": "Menerima deposit transfer bank ewallet dan pulsa"
+      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_2c3c9e17-b791-4d6e-b7f8-33194d2d5f9e_1682597219747.jpg",
+      "alt": "Selamat datang di dewata4d"
     },
     {
-      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_dd8b4b5f-a483-445a-8ad3-ba8d06d51442_1666952716797.jpg",
-      "alt": "bonus member baru 20%"
+      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_dd8b4b5f-a483-445a-8ad3-ba8d06d51442_1682660479970.jpg",
+      "alt": "bonus bergabung di dewata4d"
     },
     {
-      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_a66b64b0-ecab-4b61-859c-d8c770e5b3a3_1666952722233.jpg",
-      "alt": "bonus deposit harian 5%"
+      "url": "https://api2-d4w.tr8n2games.com/images/id_cbd_a66b64b0-ecab-4b61-859c-d8c770e5b3a3_1682660547877.jpg",
+      "alt": "link alternatif dewata4d"
     }
   ]
 }
