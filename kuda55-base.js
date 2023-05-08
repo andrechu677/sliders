06610de320f86fa61788a0b83d33636774017d0c
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-09 01:36:24 +0700

    Update kuda55-base.js

--- kuda55-base.js
+++ kuda55-base.js
@@ -1,20 +1,12 @@
 {
   "items": [
     {
-      "url": "https://159.223.50.248:9443/images/upload-Slides-20220603114551.jpg",
-      "alt": "Minimal Deposit 10ribu"
+      "url": "https://img.pay4d.info/slide-m/slidem-megagacor.jpg",
+      "alt": "daily wins slot and casino"
     },
     {
-      "url": "https://159.223.50.248:9443/images/upload-Slides-20220603114655.jpg",
-      "alt": "Promo bonus new member dan next deposit"
-    },
-    {
-      "url": "https://159.223.50.248:9443/images/upload-Slides-20220603114705.jpg",
-      "alt": "bonus rolling slot besar-besaran"
-    },
-    {
-      "url": "https://159.223.50.248:9443/images/upload-Slides-20220603114713.jpg",
-      "alt": "cashback sportsbook"
+      "url": "https://img.pay4d.info/slide-m/slidem-pg2apr.jpg",
+      "alt": "lomba thr harian"
     }
   ]
 }
