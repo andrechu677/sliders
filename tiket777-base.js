c524c27333edbb8fe9ed261871b45b45ea6c18ea
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-09 01:25:37 +0700

    Create tiket777-base.js

---
+++ tiket777-base.js
@@ -0,0 +1,12 @@
+{
+  "items": [
+    {
+      "url": "https://img.pay4d.info/slide-m/slidem-megagacor.jpg",
+      "alt": "daily wins slot and casino"
+    },
+    {
+      "url": "https://img.pay4d.info/slide-m/slidem-pg2apr.jpg",
+      "alt": "lomba thr harian"
+    }
+  ]
+}
