33ce8fead24a3066b0bf33fff1639f1a3e07ba29
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-09 01:56:12 +0700

    Create pasti777-base.js

---
+++ pasti777-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://d2luvpvg9hbilr.cloudfront.net/Banner/18/Joker%20Tournament%20Banner-ID.webp",
+      "alt": "eksklusif turnamen slot joker"
+    },
+    {
+      "url": "https://d2luvpvg9hbilr.cloudfront.net/Banner/369/slide2new(1).jpg",
+      "alt": "selamat datang di pasti777"
+    },
+    {
+      "url": "https://d2luvpvg9hbilr.cloudfront.net/Banner/369/slide1(1).jpg",
+      "alt": "minimal deposit dan withdraw 10.000 rupiah saja"
+    }
+  ]
+}
