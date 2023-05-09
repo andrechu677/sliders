cb27ac6c0e3e6c357f660777c4ef2c9fd8e8e6ab
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-10 01:04:54 +0700

    Create cinta777-base.js

---
+++ cinta777-base.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://slotcinta777.com/images/upload-Slides-20211110175628.jpg",
+      "alt": "Cinta777 - Situs Slot Online Terpercaya"
+    },
+    {
+      "url": "https://slotcinta777.com/images/upload-Slides-20211110175635.jpg",
+      "alt": "Judi Paling Murah Dengan Bonus Paling Besar"
+    },
+    {
+      "url": "https://img.pay4d.info/banner/banner-pg-lebaran.jpg",
+      "alt": "POG Lomba THR Harian"
+    }
+  ]
+}
