45f979490778bbf61c333fa589f86551749cf332
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-10 02:50:23 +0700

    Create tebar4d-base.js

---
+++ tebar4d-base.js
@@ -0,0 +1,12 @@
+{
+  "items": [
+    {
+      "url": "https://tebar4dslot.pro/images/upload-Slides-20210307154802.jpg",
+      "alt": "Selamat datang di tebar4d"
+    },
+    {
+      "url": "https://tebar4dslot.pro/images/upload-Slides-20210306192937.jpg",
+      "alt": "agen resmi betting"
+    }
+  ]
+}
