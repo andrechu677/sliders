cc77ffaa0d7ffa99d6833dbb321257db0e6b6721
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-05-11 02:17:21 +0700

    Create wabah4d-base.js

---
+++ wabah4d-base.js
@@ -0,0 +1,16 @@
+{
+	"items": [
+		{
+					"url": "https://146.190.107.143:9443/images/upload-Slides-20211202204618.jpg",
+					"alt": "Selamat datang di wabah4d"
+		},
+		{
+					"url": "https://146.190.107.143:9443/images/upload-Slides-20211202204631.jpg",
+					"alt": "judi slot paling murah dan bonus terbesar"
+        },
+		{
+					"url": "https://img.pay4d.info/banner/slide-pp-nv.jpg",
+					"alt": "Total hadiah 75 miliar pragmatic play"
+        }
+	]
+}
