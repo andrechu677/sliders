2e5a8f3ac3503647b579cedddaefe4dc4641ad8e
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-06-13 01:39:14 +0700

    Create royal777-base.js

---
+++ royal777-base.js
@@ -0,0 +1,12 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/647d796a0980f_ROYAL777-MOBILE.jpg",
+      "alt": "Promo Juni"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/647163b159b39_2.jpg",
+      "alt": "Selamat datang di ROYAL777"
+    }
+  ]
+}
