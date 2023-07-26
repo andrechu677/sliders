9168b932032a5b2c914edeed02e5c2c6d352ca92
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2023-07-27 03:51:57 +0700

    Create fafajp-base.js

---
+++ fafajp-base.js
@@ -0,0 +1,12 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/64a790414c57f_1.jpg",
+      "alt": "Menerima deposit via pulsa , emoney dan bank transfer"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/64a7905474732_2.jpg",
+      "alt": "bonus slot 100%"
+    }
+  ]
+}
