c2913a78abb98cd5258979c3cc447faf1f2253f6
andrechu677 <andrechu677@gmail.com>
2023-09-18 19:16:12 +0700

    change image url

--- alfabet188-base.js
+++ alfabet188-base.js
@@ -1,16 +1,16 @@
 {
   "items": [
     {
-      "url": "https://files.sitestatic.net/banners/644f926f7fc64_alfabet.jpg",
-      "alt": "Promo Mei Meriah Total Hadiah 90juta"
+      "url": "https://files.sitestatic.net/banners/64f7249a31f8b_alfaaaa.jpg",
+      "alt": "welcome bonus slot 100%"
     },
     {
-      "url": "https://files.sitestatic.net/banners/62fa3ea0100ae_3.jpg",
-      "alt": "Komisi dibagikan setiap hari"
+      "url": "https://files.sitestatic.net/banners/64f71fe965efb_6.jpg",
+      "alt": "welcome bonus 50% khusus permainan slot"
     },
     {
-      "url": "https://files.sitestatic.net/banners/62fa3a8346b52_4.jpg",
-      "alt": "Didukung oleh sistem deposit tercepat"
+      "url": "https://files.sitestatic.net/banners/64f71fd6b68a1_11.jpg",
+      "alt": "menerima segala jenis deposit"
     }
   ]
 }
--- asiabet118-base.js
+++ asiabet118-base.js
@@ -1,24 +1,16 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/GHzfvyMGqm8DTpZAy2rK5xHY0DHzqiS1SYAVNNcc.jpeg",
-					"alt": "Promo maret melimpah hadiah"
+					"url": "https://files.sitestatic.net/banners/6iGABgs0qaf1qb3X5ASkhHHs3T4hhXWC4CWGHVpV.jpeg",
+					"alt": "welcome bonus slot 100%"
 		},
 		{
-					"url": "https://files.sitestatic.net/banners/01kn80Hh81za1lbjUd6H9W6hIzGHjnMsIdBMKePJ.jpeg",
-					"alt": "Menerima deposit via pulsa"
+					"url": "https://files.sitestatic.net/banners/qtjaGPcM7zVpAjPqAZVcN9urCXeTtAo2OQ9iGWWE.jpeg",
+					"alt": "bonus 50% khusus permainan slot"
         },
         {
-					"url": "https://files.sitestatic.net/banners/aWiG8E0htzuwok1unfhnj05lJlz4zG19BVl489g7.jpeg",
-					"alt": "Welcome bonus slot"
-        },
-        {
-					"url": "https://files.sitestatic.net/banners/U8BhIYtGx5Ho64SiuWzIaOpPEtUNbVDoeFOme0nY.jpeg",
-					"alt": "Sistem dukungan auto deposit"
-        },
-        {
-					"url": "https://files.sitestatic.net/banners/l5HQkq6nhJYNmQMIBkXQU2yd8AajCHSbwBecFmOC.jpeg",
-					"alt": "Komisi dibagikan setiap hari"
+					"url": "https://files.sitestatic.net/banners/a61CXyXkmrUFGcO7CBvXhR8uI0Il6GPLoq6grXY2.jpeg",
+					"alt": "sistem dukungan auto deposit"
         }
 	]
 }
