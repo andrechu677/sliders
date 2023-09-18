62fac894d2d4a2abc722d6482c4aa3eec513cb87
andrechu677 <andrechu677@gmail.com>
2023-09-18 19:46:48 +0700

    changed

--- qqgalaxy-base.js
+++ qqgalaxy-base.js
@@ -1,16 +1,16 @@
 {
 	"items": [
 		{
-					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/b3f43bc667a741d0bf54114fc05dfae1.jpg",
-					"alt": "Promo Spektakuler"
+					"url": "https://qqgalaxycc.net/asset/mobile/images/banner/5324bcd7ecf9462592aa50a4833a3473.jpg",
+					"alt": "Pengumuman"
 		},
 		{
-					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/0d8f1298ee604a37870823799cca8483.jpg",
+					"url": "https://qqgalaxycc.net/asset/mobile/images/banner/aa8f0c594e5c48fd85a7af7906dd5f3e.jpg",
 					"alt": "Komisi Mingguan Terbesar"
         },
 		{
-					"url": "https://qqgalaxy99.me/Content/Web/common/images/catch/14bca79db375463b9487b45c8736e130.jpg",
-					"alt": "Proses Deposit Cepat"
+					"url": "https://qqgalaxycc.net/asset/mobile/images/banner/8e812f6b245642bf94355f3ca826db76.jpg",
+					"alt": "Menerima deposit via pulsa emoney dan transfer bank. Proses Deposit Cepat"
         }
 	]
 }
