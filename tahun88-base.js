b6eb31cc358c30100e4a9507e02b3da0f34efdc8
andrechu677 <andrechu677@gmail.com>
2023-09-19 23:16:15 +0700

    url changed

--- tahun88-base.js
+++ tahun88-base.js
@@ -1,16 +1,17 @@
 {
 	"items": [
-		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_ab151be8-9a06-4ac9-a954-bb6b05a0c29d_1663942259810.jpg",
-					"alt": "Selamat datang di tahun88"
-		},
-		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_63b6dd26-5457-4a4c-8645-1a14b4922fce_1663943206150.jpg",
-					"alt": "Transaksi mudah dan cepat"
-        },
-		{
-					"url": "https://api2-tah.tr8ngames.com/images/id_cbd_96792d66-e28e-43fb-b7cb-b9e4abf4abe2_1663943296300.jpg",
-					"alt": "Promo Slot Menang kalah Dapet Duit"
-        }
+	  {
+		"url": "https://api2-tah.imgnxa.com/images/id_cbm_ab151be8-9a06-4ac9-a954-bb6b05a0c29d_1663942259810.jpg",
+		"alt": "situsnya para crazy rich"
+	  },
+	  {
+		"url": "https://api2-tah.imgnxa.com/images/id_cbm_63b6dd26-5457-4a4c-8645-1a14b4922fce_1663943206150.jpg",
+		"alt": "transaksi mudah dan cepat"
+	  },
+
+	  {
+		"url": "https://api2-tah.imgnxa.com/images/id_cbm_96792d66-e28e-43fb-b7cb-b9e4abf4abe2_1663943296300.jpg",
+		"alt": "promo slot menang kalah dapat duit"
+	  }
 	]
-}
+  }
