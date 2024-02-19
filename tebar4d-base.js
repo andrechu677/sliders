f5ffbc6e80933c1b655b111ee6cb2196111d36f9
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2024-02-19 21:26:52 +0700

    Update tebar4d-base.js

--- tebar4d-base.js
+++ tebar4d-base.js
@@ -1,12 +1,8 @@
 {
   "items": [
     {
-      "url": "https://tebar4dslot.pro/images/upload-Slides-20210307154802.jpg",
+      "url": "https://tebar4dslot.biz/images/upload-MobileSlides-20231112142846.png",
       "alt": "Selamat datang di tebar4d"
-    },
-    {
-      "url": "https://tebar4dslot.pro/images/upload-Slides-20210306192937.jpg",
-      "alt": "agen resmi betting"
     }
   ]
 }
