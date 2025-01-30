7b305db82c0a314a261b1d274ce785d2674d1cd5
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:33:38 +0700

    Update ratuking4d-base.js

--- ratuking4d-base.js
+++ ratuking4d-base.js
@@ -1,10 +1,6 @@
 {
 	"items": [
 		{
-					"url": "https://www.ratukingco.info/assets/banner/IND/ratuking_20215636115636.jpg",
-					"alt": "Menerima depopsit via pulsa"
-		},
-		{
 					"url": "https://www.ratukingco.info/assets/banner/IND/komisi-slot_20211255141255.jpg",
 					"alt": "Komisi Mingguan Slot Game"
         },
