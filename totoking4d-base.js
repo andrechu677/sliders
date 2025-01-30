ed0472daadaa1ff46dd5bdf4a49f8a19ab35e8cf
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:31:25 +0700

    Update totoking4d-base.js

--- totoking4d-base.js
+++ totoking4d-base.js
@@ -7,10 +7,6 @@
     {
       "url": "https://www.totokingac.com/assets/banner/IND/cashback-ayam-ikan-dingdong_20212658052658.jpg?V=1622957225",
       "alt": "bonuss cashback mingguan"
-    },
-    {
-      "url": "https://www.totokingac.com/assets/banner/IND/totoking_20215329115329.jpg?V=1631361214",
-      "alt": "menerima deposit via pulsa"
     }
   ]
 }
