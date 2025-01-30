4c63298400aa1215503ee6f0068f1f7889a8f660
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:34:13 +0700

    Update royal188-base.js

--- royal188-base.js
+++ royal188-base.js
@@ -7,10 +7,6 @@
     {
       "url": "https://files.sitestatic.net/banners/cYptDurg0kgSV389RXbqsTnnJNnvCB11P4WdYUCl.jpg",
       "alt": "komisi dibagikan setiap hari"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/y5g2MbpLXONkWZHTWDE9DT3R6azhEyFuWBzv0MiI.jpg",
-      "alt": "menerima segala jenis deposit"
     }
   ]
 }
