7240671785bb6f45fedd3c0108e73ac5aa181c24
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:30:17 +0700

    Update ludo4d-base.js

--- ludo4d-base.js
+++ ludo4d-base.js
@@ -5,10 +5,6 @@
       "alt": "Selamat datang di ludo4d"
     },
     {
-      "url": "https://157.245.197.62:9443/images/upload-Slides-20230308115935.jpg",
-      "alt": "Deposit pulsa tanpa potongan"
-    },
-    {
       "url": "https://157.245.197.62:9443/images/upload-Slides-20230308115953.jpg",
       "alt": "Judi online paling murah"
     }
