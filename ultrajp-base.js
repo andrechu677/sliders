a4b65e8963e87008d6da3b697cf36e8eee8baafa
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:30:44 +0700

    Update ultrajp-base.js

--- ultrajp-base.js
+++ ultrajp-base.js
@@ -5,11 +5,6 @@
       "alt": "Welcome bonus slot"
     },
     {
-      "url": "https://files.sitestatic.net/banners/64f725fc81630_mobile-deposit.jpg",
-      "alt": "menerima segala sistem deposit"
-    },
-
-    {
       "url": "https://files.sitestatic.net/banners/64f72c5038f82_17.jpg",
       "alt": "komisi dibagikan setiap hari"
     }
