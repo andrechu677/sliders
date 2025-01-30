cf8905ec808d26079b553953568ff1f610cd8191
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:31:08 +0700

    Update ultra777-base.js

--- ultra777-base.js
+++ ultra777-base.js
@@ -5,10 +5,6 @@
       "alt": "komisi dibagikan setiap hari"
     },
     {
-      "url": "https://files.sitestatic.net/banners/64f7243d1570f_9.jpg",
-      "alt": "Menerima deposit via pulsa"
-    },
-    {
       "url": "https://files.sitestatic.net/banners/62eb9f697924f_ultra777.jpg",
       "alt": "Welcome Bonus slot 100%"
     }
