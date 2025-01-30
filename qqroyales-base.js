6affbc754666cef2f6e9e059e19316ee5a029a24
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:33:11 +0700

    Update qqroyales-base.js

--- qqroyales-base.js
+++ qqroyales-base.js
@@ -1,13 +1,6 @@
 {
 	"items": [
-		{
-					"url": "https://qqroyalai.com/asset/mobile/images/banner/599c1853f59740328a162fdcf6e0d49c.jpg",
-					"alt": "Dukungan sistem auto deposit"
-		},
-		{
-					"url": "https://qqroyalai.com/asset/mobile/images/banner/a258d28cf1814d1095871fe3ffec8563.jpg",
-					"alt": "Minimal deposit 10rb"
-        },
+		
 		{
 					"url": "https://qqroyalai.com/asset/mobile/images/banner/5a5589902512462eaeb3b57dca4d3a3b.jpg",
 					"alt": "Komisi setiap minggu dari qqroyal. Paling banyak bonusnya"
