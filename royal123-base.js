cfa8beca7a0d280cbb989f5b415e6b4d7be19dc1
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:33:49 +0700

    Update royal123-base.js

--- royal123-base.js
+++ royal123-base.js
@@ -1,10 +1,6 @@
 {
 	"items": [
 		{
-					"url": "https://files.sitestatic.net/banners/64f70a8970a82_12.jpg",
-					"alt": "Menerima segala jenis deposit"
-		},
-		{
 					"url": "https://files.sitestatic.net/banners/64f70acf7df7d_3.jpg",
 					"alt": "cashback mingguan"
         },
