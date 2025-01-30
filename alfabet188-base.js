42ca29d6723c8a4f0def5c0a5cce98cd7ae00dbf
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:29:13 +0700

    Update alfabet188-base.js

--- alfabet188-base.js
+++ alfabet188-base.js
@@ -7,10 +7,6 @@
     {
       "url": "https://files.sitestatic.net/banners/64f71fe965efb_6.jpg",
       "alt": "welcome bonus 50% khusus permainan slot"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/64f71fd6b68a1_11.jpg",
-      "alt": "menerima segala jenis deposit"
     }
   ]
 }
