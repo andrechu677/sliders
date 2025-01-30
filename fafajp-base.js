497c78c36aca90ca4a494975b2a3569b6a3e4f3c
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:29:48 +0700

    Update fafajp-base.js

--- fafajp-base.js
+++ fafajp-base.js
@@ -1,10 +1,6 @@
 {
   "items": [
     {
-      "url": "https://files.sitestatic.net/banners/64f71d3dde0c9_mobile-deposit.jpg",
-      "alt": "Menerima deposit via pulsa , emoney dan bank transfer"
-    },
-    {
       "url": "https://files.sitestatic.net/banners/64f71dc12669b_MOBILE-CASH.jpg",
       "alt": "cashback mingguan 10%"
     },
