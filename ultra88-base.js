3ba8c2424935a617085afbbbe89eea9195d7b514
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:28:57 +0700

    Update ultra88-base.js

--- ultra88-base.js
+++ ultra88-base.js
@@ -7,10 +7,6 @@
     {
       "url": "https://files.sitestatic.net/banners/bGPomc3lAlQvdDwXD6RX0d1oMENKOJFg14RUWJ0f.jpeg",
       "alt": "bonus cashback mingguan 10%"
-    },
-    {
-      "url": "https://files.sitestatic.net/banners/w8lhWd0C1yMv0uLv4pAt5Acs94gbYkOkb0N7cW7i.jpeg",
-      "alt": "menerima segala jenis sistem deposit"
     }
   ]
 }
