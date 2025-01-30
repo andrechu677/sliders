af78c592964c2e19a1eb83a57e77784d576ae1ba
andrechu677 <89152615+andrechu677@users.noreply.github.com>
2025-01-30 15:32:50 +0700

    Update qqgalaxy-base.js

--- qqgalaxy-base.js
+++ qqgalaxy-base.js
@@ -7,10 +7,6 @@
 		{
 					"url": "https://qqgalaxycc.net/asset/mobile/images/banner/aa8f0c594e5c48fd85a7af7906dd5f3e.jpg",
 					"alt": "Komisi Mingguan Terbesar"
-        },
-		{
-					"url": "https://qqgalaxycc.net/asset/mobile/images/banner/8e812f6b245642bf94355f3ca826db76.jpg",
-					"alt": "Menerima deposit via pulsa emoney dan transfer bank. Proses Deposit Cepat"
         }
 	]
 }
